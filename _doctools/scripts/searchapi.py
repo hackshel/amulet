a9fbae77cf8154dc6be9bdce41add2dcb9ef5a4e
dainan13@gmail.com <dainan13@gmail.com@3753b899-bdd1-54c5-3867-196bee7fa449>
2011-04-07 10:10:21 +0000

    write codes for doctools. not fin.

--- _doctools/scripts/searchapi.py
+++ _doctools/scripts/searchapi.py
@@ -1,4 +1,5 @@
 import os.path
+import tempfile
 
 
 class SearchAPI ( object ):
@@ -8,7 +9,13 @@ class SearchAPI ( object ):
         self.path = path
         self.fileexts = ['.c','.cpp','.h','.hpp']
         
-    def parser( self, name ):
+    @staticmethod
+    def findjust( lns ):
+        doc = [ d.lstrip() for d in doc ]
+        return min( (len(d) - len(d.lstrip())) 
+                     for d in doc if len(d) != 0 )
+        
+    def parser( self, arg, name ):
         
         with open( name, 'r' ) as fp :
             
@@ -18,29 +25,46 @@ class SearchAPI ( object ):
             while( i < len(lns) ):
                 
                 if lns[i].startswith('/*') \
-                                and a.strip('/*').lower() = 'autodoc' :
+                                and a.strip('/* ').lower().startswith('autodoc ') :
                     
                     j = i
                     while( j < len(lns) and '*/' not in lns[j] ):
                         pass
                     
                     if i != j :
-                        doc = [ lns[i].split('/*',1)[-1] ]\
+                        doc = [ lns[i].split('/*',1)[-1].split('autodoc ')[-1] ]\
                               + lns[i+1:j-1]
                               + [lns[j].split('*/',1)[0] ]
                     else :
-                        doc = lns[i].split('/*',1)[-1].split('*/',1)[0]
-            
-                    doc = '' + '\r\n'.join(doc)
+                        doc = lns[i].split('/*',1)[-1].split('autodoc ')[-1]\
+                                                      .split('*/',1)[0]
+                    
+                    doc = [ d.expandtabs(4).rstrip() for d in doc ]
+                    ljust_len = self.findjust( doc )
+                    if ljust_len != 0 :
+                        doc = [ d[ljust_len:] for d in doc ]
+                    
+                    while( len(doc) > 0 and doc[0] == '' ):
+                        doc.pop(0)
+                    while( len(doc) > 0 and doc[-1] == '' ):
+                        doc.pop(-1)
+                        
+                    doc = '' + '\r\n'.join( for d in doc )
                     
                     i = j
                     while( j < len(lns) and '{' in lns[j] ):
                         pass
                     
                     fdef = lns[i:j-1] + [lns[j].split('{')[0]]
-                    fdef = ''.join(fdef)
+                    fdef = ''.join( [ fe.strip() for fe in fdef ] )
+                    
+                    arg.append( 
+                        '.. c:function::' + fdef + '\r\n' + ''
+                    )
                     
                     i = j+1
+                    
+                    
                     
         
         return
@@ -59,7 +83,7 @@ class SearchAPI ( object ):
             
             if os.path.isfile(fn) :
                 
-                self.parser(fn)
+                self.parser( arg, fn )
             
         return
         
