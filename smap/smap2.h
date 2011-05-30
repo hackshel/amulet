30f1f9b5838be6bd2a2f7d899e4ae290c74bf1b1
nayuhz@gmail.com <nayuhz@gmail.com@3753b899-bdd1-54c5-3867-196bee7fa449>
2011-05-30 12:29:45 +0000

    Began to support string types

--- smap/smap2.h
+++ smap/smap2.h
@@ -4,6 +4,7 @@
 
 
 struct smap {
+	int hash_type;
 	uint32_t	bucket_num;
 	uint32_t	seg_num;
 	int seg_shift;
@@ -12,15 +13,28 @@ struct smap {
 	struct SEGMENT *seg;	
 };
 
+struct PAIR {
+	int type; /* number or string */
+	int key_len;
+	union {
+		uint64_t	ikey;
+		char	*skey;
+	};
+	void *data;
+};
+
 struct smap *smap_init(int, int, int, int);
-int smap_insert(struct smap *, uint64_t, void *, int);
+int smap_insert(struct smap *, struct PAIR *, int);
 int smap_delete(struct smap *, uint64_t, int);
 void *smap_get(struct smap *, uint64_t);
 int smap_update(struct smap *, uint64_t, void *);
-int smap_traverse(struct smap *, int (*routine)(struct smap *, uint64_t, void *),uint32_t);
+int smap_traverse(struct smap *, int (*routine)(struct smap *, struct PAIR *),uint32_t);
 void smap_key_lock(struct smap *, uint64_t);
 int smap_key_unlock(struct smap *, uint64_t);
 uint64_t smap_get_elm_num(struct smap *);
+
+#define HASHTYPE_NUM 0
+#define HASHTYPE_STR 1
 
 #define SMAP_BREAK 1
 #define SMAP_GENERAL_ERROR -1
