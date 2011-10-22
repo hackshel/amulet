4abd0f7fac80e90691601463790dd9ced3b52601
dainan13@gmail.com <dainan13@gmail.com@3753b899-bdd1-54c5-3867-196bee7fa449>
2011-10-22 05:00:38 +0000

    rewrite the socket_oo, provide a OO socket address struct;

--- libnet_am/socket_oo.c
+++ libnet_am/socket_oo.c
@@ -10,13 +10,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include "networks.h"
+#include "socket_oo.h"
 
 
 SockAddrObj* SockAddrObj_fromfd( SockAddrObj* self, int fd )
 {
     int sockaddrlen = sizeof(struct sockaddr_in);
     
+    // create SockAddrObj if self is NULL
     if ( self == NULL )
     {
         self = (SockAddrObj*)malloc(sizeof(SockAddrObj));
@@ -24,21 +25,84 @@ SockAddrObj* SockAddrObj_fromfd( SockAddrObj* self, int fd )
             return NULL;
     }
     
+    // fill SockAddrObj Info
     if (getsockname( fd, (struct sockaddr *)&(self->addr), &sockaddrlen ) == 0)
         return self;
     
     return NULL;
 }
 
-SockAddrObj* SockAddrObj_fromstring( SockAddrObj* self, char* address, int timeout );
+
+SockAddrObj* SockAddrObj_fromstring( SockAddrObj* self, char* address, int len, long defaultport )
 {
     int sockaddrlen = sizeof(struct sockaddr_in);
     
     int i;
-    char* port_p;
-    char domain[255];
+    char* c; 
+    char* endaddr;
+    char domain[256];
     struct hostent *hp;
+    long port;
+
     
+    // parse the address
+    if ( len >= 256 )
+        return NULL;
+    
+    endaddr = address + ( ( len < 0 ) ? 256 : (len+1) );
+    
+    for ( c = address; *c != '\0' && *c != ':' && c < endaddr ; c++ );
+    
+    if ( *c == ':' )
+    {
+        if ( c[1] == '\0' ) return NULL;
+        
+        port = strtol( c+1, NULL, 10 );
+        
+        if ( port == LONG_MIN || port == LONG_MAX ) return NULL;
+    }
+    else
+    {
+        port = defaultport;
+        
+        if ( port < 0 || port >= 65536 ) return NULL;
+    }
+    
+    i = c-address-1;
+    strncpy( domain, address, i );
+    domain[i] = '\0';
+
+    hp = gethostbyname( domain );
+    if ( hp == NULL || hp->h_length <= 0 ) return NULL;
+
+
+    // create SockAddrObj if self is NULL
+    if ( self == NULL )
+    {
+        self = (SockAddrObj*)malloc(sizeof(SockAddrObj));
+        if ( self == NULL )
+            return NULL;
+    }
+
+    
+    // fill SockAddrObj Info
+    strcpy( self->hostname, hp->h_name );
+    
+    self->addr.sin_family = AF_INET;
+    self->addr.sin_port = htons( port );
+    self->addr.sin_addr.s_addr = *((unsigned long *)hp->h_addr_list[0]);
+    bzero( (void*)(&(self->addr.sin_zero)), 8 );
+    
+    return self;
+}
+
+
+SockAddrObj* SockAddrObj_frombinary( SockAddrObj* self, char* p, char* h )
+{
+    
+    if ( h == NULL ) h = p + 2;
+    
+    // create SockAddrObj if self is NULL
     if ( self == NULL )
     {
         self = (SockAddrObj*)malloc(sizeof(SockAddrObj));
@@ -46,245 +110,161 @@ SockAddrObj* SockAddrObj_fromstring( SockAddrObj* self, char* address, int timeo
             return NULL;
     }
     
-    for ( i = 0, port_p = p ; 
-          *port_p != '\0' && ( len == 0 || i < len ) ; 
-          i++, port_p++ )
-    {
-        if ( *port_p == ':' )
-        {
-            break;
-        }
-    }
+    // fill SockAddrObj Info
+    self->addr.sin_family = AF_INET;
+    
+    memcpy( &(self->addr.sin_port), p, 2 );
+    memcpy( &(self->addr.sin_addr.s_addr), h, 4 );
+    
+    bzero( (void*)(&(self->addr.sin_zero)), 8 );
+    
+    return self;
 }
 
 
-int ep_address_fromstring( char* p, int len, int defaultport, ep_address* epaddr )
-{
-    int i ;
-    char* port_p;
-    char domain[255];
-    struct hostent *hp;
-    
-    for ( i = 0, port_p = p ; 
-          *port_p != '\0' && ( len == 0 || i < len ) ; 
-          i++, port_p++ )
-    {
-        if ( *port_p == ':' )
-        {
-            break;
-        }
-    }
-    
-    if ( i > 255 )
-    {
-        return -1;
-    }
-    
-    if (  *port_p == ':' )
-    {
-        port_p ++;
-        
-        if ( *port_p == '\0' )
-        {
-            return -1;
-        }
-        
-        defaultport = strtol( port_p, NULL, 10 );
-        
-    }
-    else
-    {
-        if ( defaultport == 0 )
-        {
-            return -1;
-        }
-    }
-    
-    //printf( " i: %d", i );
-    
-    strncpy( domain, p, i );
-    domain[i] = '\0';
-    
-    //puts(domain);
-    
-    hp = gethostbyname( domain );
-    
-    if ( hp == NULL || hp->h_length <= 0 )
-    {
-        return -1;
-    }
-    
-    //strcpy( hp->h_name, epaddr->hostname );
-    strcpy( epaddr->hostname, hp->h_name );
-    
-    
-    epaddr->addr.sin_family = AF_INET;
-    if ( defaultport == -1 )
-    {
-        memcpy( &(epaddr->addr.sin_port), p+len, 2 );
-    }
-    else
-    {
-        epaddr->addr.sin_port = htons( defaultport );
-    }
-    epaddr->addr.sin_addr.s_addr = *((unsigned long *)hp->h_addr_list[0]);
-    
-    //inet_pton( AF_INET, addr, &(p_remote_addr->sin_addr) );
-    
-    bzero( (void*)(&(epaddr->addr.sin_zero)), 8 );
-    
-    return 0;
-}
 
-int ep_address_inaddr_any( int port, ep_address* epaddr  )
-{
-    epaddr->hostname[0] = '\0';
-    
-    epaddr->addr.sin_family = AF_INET;
-    epaddr->addr.sin_port = htons( port );
-    epaddr->addr.sin_addr.s_addr = INADDR_ANY;
-    
-    bzero( (void*)(&(epaddr->addr.sin_zero)), 8 );
-    
-    return 0;
-}
 
-int ep_address_frombinary( char* h, char* p, ep_address* epaddr )
-{
-    epaddr->addr.sin_family = AF_INET;
-    
-    memcpy( &(epaddr->addr.sin_port), p, 2 );
-    memcpy( &(epaddr->addr.sin_addr.s_addr), h, 4 );
-    
-    bzero( (void*)(&(epaddr->addr.sin_zero)), 8 );
-    
-    return 0;
-}
-
-int ep_address_fromfd( int fd, ep_address* epaddr  )
-{
-    
-    int len = sizeof( struct sockaddr_in );
-    
-    getsockname( fd, ( struct sockaddr *) &(epaddr->addr), &len );
-    
-    return 0;
-}
-
-int ep_address_tobinary( ep_address* epaddr, char* p, char* h )
-{
-    //memcpy( p, &(epaddr->addr.sin_port), 6 );
-    
-    memcpy( p , &(epaddr->addr.sin_port), 2 );
-    
-    if ( h == NULL )
-    {
-        memcpy( p+2 , &(epaddr->addr.sin_addr.s_addr), 4 );
-    }
-    else
-    {
-        memcpy( h , &(epaddr->addr.sin_addr.s_addr), 4 );
-    }
-    
-    return 0;
-}
-
-int ep_address_tostring_pretty( ep_address* epaddr, char* p, int size )
+char* SockAddrObj_tostring( SockAddrObj* self, char* address, int maxlength )
 {
     int i = 0;
     
-    if ( epaddr->hostname[0] != '\0' )
-    {
-        strncpy( p, epaddr->hostname, size );
-        i = strlen( epaddr->hostname );
-        p[i] = ' ';
-        i ++ ;
-        size = size - i - 1;
-    }
+    if ( self->hostname[0] != '\0' )
+        i = strlen( strncpy( address, self->hostname, maxlength ) );
+    else
+        i = strlen( inet_ntop(AF_INET, &(self->addr.sin_addr), address, maxlength) );
     
-    inet_ntop(AF_INET, &(epaddr->addr.sin_addr), p+i, size);
+    if ( i == maxlength ) return address;
     
-    i += strlen( p+i );
+    snprintf( address+i, maxlength-i, ":%d", ntohs(self->addr.sin_port) );
     
-    sprintf( p+i, ":%d", ntohs(epaddr->addr.sin_port) );
-    
-    return 0;
+    return address;
 }
 
-int create_listen_socket( ep_address* epaddr )
+
+char* SockAddrObj_tobinary( SockAddrObj* self, char* p, char* h )
 {
+    if ( h == NULL ) h = p + 2;
+    
+    memcpy( p , &(self->addr.sin_port), 2 );
+    memcpy( h , &(self->addr.sin_addr.s_addr), 4 );
+    
+    return p;
+}
+
+
+char g_SockAddrObj_errorbox[][50] = {
+    "Not Defined Error: ",
+    "Create Socket Error (socket): ",
+    "Manipulating Socket Options Error (setsockopt): ",
+    "Bind Socket Error (bind): ",
+    "Listen Socket Error (listen): ",
+};
+
+char* SockAddrObj_printerror( SockAddrObj* self, int errno, char* msgbuf, int maxlength )
+{
+    
+    int i = 0;
+    char* tp = msgbuf;
+    
+    if ( errno > 0 || errno <= -5 ) errno = 0;
+    errno = -errno;
+    
+    i = strlen( strncpy( tp, g_SockAddrObj_errorbox[errno], maxlength ) );
+    
+    if ( i == maxlength )
+        return msgbuf;
+    else
+        tp = tp + i;
+        maxlength = maxlength-i;
+
+    if ( self->hostname[0] != '\0' )
+        i = strlen( strncpy( tp, self->hostname, maxlength ) );
+
+    if ( i == maxlength )
+        return msgbuf;
+    else
+        tp = tp + i;
+        maxlength = maxlength-i;
+
+    i = strlen( inet_ntop(AF_INET, &(self->addr.sin_addr), tp, maxlength) );
+    
+    if ( i == maxlength )
+        return msgbuf;
+    else
+        tp = tp + i;
+        maxlength = maxlength-i;
+    
+    snprintf( tp, maxlength, ":%d", ntohs(self->addr.sin_port) );
+    
+    return msgbuf;
+}
+
+
+int SockAddrObj_bind_and_listen( SockAddrObj* self )
+{
+    int sockaddrlen = sizeof(struct sockaddr_in);
+    
     int listen_fd;
-    int flag;
+    int flag, flags;
     
     if ( ( listen_fd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 )
-    {
-        perror("create socket error");
-        exit(1);
-    }
+        return -1;
     
     if ( setsockopt( 
             listen_fd, SOL_SOCKET, SO_REUSEADDR, 
             (char *)&flag, sizeof(flag) ) == -1 )
-    {
-        perror("setsockopt error");
-    }
+        return -2;
     
-    int flags = fcntl(listen_fd, F_GETFL, 0);
+    flags = fcntl(listen_fd, F_GETFL, 0);
     
     fcntl(listen_fd, F_SETFL, flags|O_NONBLOCK);
     
-    //bzero( (void*)(&(epaddr->addr.sin_zero)), 8 );
-    
-    if ( bind( listen_fd, (struct sockaddr *)&(epaddr->addr), g_addr_len ) == -1 )
-    {
-        perror("bind error");
-        exit(1);
-    }
+    if ( bind( listen_fd, (struct sockaddr *)&(self->addr), sockaddrlen ) == -1 )
+        return -3;
     
     if ( listen( listen_fd, 1 ) == -1 )
-    {
-        perror("listen error");
-        exit(1);
-    }
+        return -4;
     
     return listen_fd;
 }
 
 
-int accept_socket( int listen_fd, ep_address* epaddr )
+SockAddrObj* SockAddrObj_accept( SockAddrObj* self, int listen_fd, int* p_accept_fd )
 {
-    int accept_fd ;
-    int flags ;
+    int sockaddrlen = sizeof(struct sockaddr_in);
     
-    //bzero( (void*)(&(epaddr->addr.sin_zero)), 8 );
+    int accept_fd;
+    int flags;
     
-    accept_fd = accept( listen_fd, (struct sockaddr *)&(epaddr->addr), 
-                        &g_addr_len );
+    // create SockAddrObj if self is NULL
+    if ( self == NULL )
+    {
+        self = (SockAddrObj*)malloc(sizeof(SockAddrObj));
+        if ( self == NULL )
+            return NULL;
+    }
     
+    accept_fd = accept( listen_fd, (struct sockaddr *)&(self->addr), &sockaddrlen );
     flags = fcntl(accept_fd, F_GETFL, 0);
-    
     fcntl( accept_fd, F_SETFL, flags|O_NONBLOCK );
     
-    epaddr->hostname[0] = '\0';
+    self->hostname[0] = '\0';
     
-    return accept_fd;
+    *p_accept_fd = accept_fd;
+    
+    return self;
 }
 
-
-int connect_socket( ep_address* epaddr )
+int SockAddrObj_connect( SockAddrObj* self, int timeout )
 {
+    int sockaddrlen = sizeof(struct sockaddr_in);
+    
     int connect_fd;
     
     connect_fd = socket(AF_INET, SOCK_STREAM, 0);
     
-    if ( connect( connect_fd, 
-                  (struct sockaddr *)&(epaddr->addr), 
-                  sizeof(struct sockaddr_in)
-            ) == -1 )
-    {
-        printf( "connection error (%d)", errno );
-        return -1;
-    }
+    if ( connect( connect_fd, (struct sockaddr *)&(self->addr), sockaddrlen ) == -1 )
+        return -5;
     
     return connect_fd;
 }
